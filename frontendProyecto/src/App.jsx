81280ec4fbf1f2207a3d97a1d2cd98b14edae447
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2024-05-21 17:17:37 -0400

    prototipo de Navbar mas Rutas terminado- falta logica de carrito y buscador

--- frontendProyecto/src/App.jsx
+++ frontendProyecto/src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { route, routes, navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
+import Navbar from './components/Navbar'
 
 function App() {
-  <Routes>
-    <Route path="/" element={token ? <HomePage /> : <Navigate to="/login" />} />
-    <Route
-      path="/login"
-      element={token ? <Navigate to="/" /> : <LoginPage />}
-    />
-    <Route path="/register" element={<RegisterPage />} />
 
-    <Route path="/perfil" element={<Perfil />} />
-    <Route path="/carrito" element={<Carrito />} />
-    <Route path="/vistadetalle" element={<VistaDetalle />} />
-  </Routes>;
+  return (
+    <>
+      <Navbar />
+      {/* <Routes>
+        <Route path="/" element={token ? <HomePage /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/" /> : <LoginPage />}
+        />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/vistadetalle" element={<VistaDetalle />} />
+      </Routes>; */}
+    </>
+  )
+
 }
 
 export default App;
--- frontendProyecto/src/components/Navbar.jsx
+++ frontendProyecto/src/components/Navbar.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react'
+import '../assets/css/navStyle.css'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-  return <div>Navbar</div>;
-};
+  
+  const isActiveNav = ({isActive}) =>{
+    return isActive ? "active" : "notActive"
+  }
+  
+  return (
+    <nav>
+      <NavLink className="icono" to= "/">
+        Icono
+      </NavLink>
+      <input className='buscador' type="text" placeholder='Buscar' />
+      <div className="opciones">
+        <NavLink className={isActiveNav} to= "/login">
+          log in
+        </NavLink>
+        <NavLink className={isActiveNav} to= "/register">
+          Register
+        </NavLink>
+        <NavLink className={isActiveNav} to= "/cart">
+          Cart
+        </NavLink>
+      </div>
+    </nav>
+  )
+}
 
-export default Navbar;
+export default Navbar
