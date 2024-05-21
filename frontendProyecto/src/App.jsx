8930223043622754db5dd50c9dae021333d637f4
jcorp11 <joaco.011@gmail.com>
2024-05-21 19:28:27 -0400

    arreglo un poco app.jsx

--- frontendProyecto/src/App.jsx
+++ frontendProyecto/src/App.jsx
@@ -1,27 +1,24 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import {Home} from "./views/Home";
+import { Home } from "./views/Home";
+import { LoginPage } from "./views/LoginPage";
 
 function App() {
-
   return (
     <>
       <Home />
-      {/* <Routes>
-        <Route path="/" element={token ? <HomePage /> : <Navigate to="/login" />} />
-        <Route
-          path="/login"
-          element={token ? <Navigate to="/" /> : <LoginPage />}
-        />
-        <Route path="/register" element={<RegisterPage />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/register" element={<RegisterPage />} />
 
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/carrito" element={<Carrito />} />
-        <Route path="/vistadetalle" element={<VistaDetalle />} />
-      </Routes>; */}
+        <Route path="/vistadetalle" element={<VistaDetalle />} /> */}
+      </Routes>
+      ;
     </>
-  )
-
+  );
 }
 
 export default App;
