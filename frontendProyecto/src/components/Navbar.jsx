600a05d866943129e41a636a9038af7edc79232c
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2024-05-23 03:25:23 -0400

    Se termino la logica de buscador, Actualmente funcionando

--- frontendProyecto/src/components/Navbar.jsx
+++ frontendProyecto/src/components/Navbar.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useContext, useMemo } from "react";
 import "../assets/css/navStyle.css";
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
 import { productosContext } from "../context/ProductProvider";
 import Validacion from "./Validacion";
+import { galeriaContext } from "../context/GaleriaProvider.jsx";
 
 const Navbar = () => {
   const { precioTotal } = useContext(productosContext);
+  const { setGaleriaData, dataOriginal } = useContext(galeriaContext);
+  const [inputBuscador, setInputBuscador] = useState("");
+
+  // Función de debounce
+  function debounce(func, delay) {
+    let timeoutId;
+    return function (...args) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  }
+
+  const debouncedFilter = useMemo(
+    () =>
+      debounce((inputValue) => {
+        if (inputValue === "") {
+          setGaleriaData(dataOriginal);
+        } else {
+          const filtro = dataOriginal.filter((element) =>
+            element.titulo.toLowerCase().includes(inputValue)
+          );
+          setGaleriaData(filtro);
+        }
+      }, 300), // 300ms de espera antes de llamar a la función
+    [dataOriginal]
+  );
+
+  function filtrarProductos(e) {
+    const inputValue = e.target.value.toLowerCase();
+    setInputBuscador(inputValue);
+    debouncedFilter(inputValue);
+  }
 
   const isActiveNav = ({ isActive }) => {
     return isActive ? "active" : "notActive";
@@ -15,21 +51,28 @@ const Navbar = () => {
   return (
     <nav>
       <Validacion condition={true}>
-        {/* Validacion */}
         <NavLink className="icono" to="/">
           Icono
         </NavLink>
       </Validacion>
-      <input className="buscador" type="text" placeholder="Buscar" />
+      <div className="search-container">
+        <input
+          className="buscador"
+          type="text"
+          placeholder="Buscar"
+          onChange={filtrarProductos}
+          value={inputBuscador}
+        />
+      </div>
       <div className="opciones">
         <NavLink className={isActiveNav} to="/login">
-          log in
+          Iniciar Sesión
         </NavLink>
         <NavLink className={isActiveNav} to="/register">
-          Register
+          Registrarse
         </NavLink>
         <NavLink className={isActiveNav} to="/cart">
-          Cart 🛒 {precioTotal}
+          Carrito 🛒 {precioTotal}
         </NavLink>
       </div>
     </nav>
@@ -37,3 +80,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
